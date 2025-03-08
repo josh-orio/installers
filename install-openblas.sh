fb8ad35ccffb4cb74ed6d11717dcd6b5f8b49803
Joshua <46853849+josh-orio@users.noreply.github.com>
2025-03-08 23:11:26 +0000

    Improves OpenBLAS installer - cleanup

--- install-openblas.sh
+++ install-openblas.sh
@@ -6,4 +6,16 @@
 git clone https://github.com/OpenMathLib/OpenBLAS.git
 cd OpenBLAS
 mkdir build && cd build
-cmake .. && sudo make install
+cmake .. && sudo make -j install
+
+cd ../..
+
+read -p "delete build directory (openblas) (y/n): " del
+
+if [[ "$del" == "y" || "$del" == "Y" ]]; then
+	rm -rf OpenBLAS
+elif [[ "$del" == "n" || "$del" == "N" ]]; then
+	echo "ignoring build directory"
+else
+	echo "unrecognized input!"
+fi
